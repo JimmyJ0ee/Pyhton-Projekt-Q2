43de0136f1414620faf2215db71d0717814618d5
JimmyJ0ee <98901860+JimmyJ0ee@users.noreply.github.com>
2022-04-30 09:45:42 +0200

    changes in test_files: main, dices, file_handling

--- kniffel_chris/test_dice_functions.py
+++ kniffel_chris/test_dice_functions.py
@@ -1,34 +1,35 @@
-from io import StringIO
 from unittest import TestCase
+from dice_functions import dices, dice_new
 from unittest.mock import patch
-import dice_functions
+from io import StringIO
 
-class TestDiceFunctions (TestCase):
+class Test_dices_functions(TestCase):
 
     def test_dice_new(self):
         action_numbers = [1,2,3]
         with patch('sys.stdin', new = StringIO()):
-            self.assertRaises(Exception, dice_functions.dice_new(action_numbers))
+            self.assertRaises(Exception, dice_new(action_numbers))
 
     #klappt nicht: eof line read bei sys.stdin
     #def test_dices(self):
     #    with patch('sys.stdin', new = StringIO('1\n')):
     #        self.assertRaises(Exception, dice_functions.dices(0))
 
+
     def test_dices(self):
         with patch('sys.stdin', new = StringIO('1\n2\n3')):
             with self.assertRaises(IndexError):
-                dice_functions.dices(3)
+                dices(3)
         with patch('sys.stdin', new = StringIO('1\n2\n3')):
             with self.assertRaises(TypeError):
-                dice_functions.dices('ABC')
+                dices('ABC')
         with patch('sys.stdin', new = StringIO('1\n2\n3')):
             with self.assertRaises(TypeError):
-                dice_functions.dices(0.5458558)        
-            #Programm läuft bei korrekter Eingabe durch     
+                dices(0.5458558)        
+#Programm läuft bei korrekter Eingabe durch     
         with patch('sys.stdin', new = StringIO('1\n2\n3')):
-            self.assertRaises(Exception, dice_functions.dices(0))
-            #Falscheingabe wird abgefangen - Programm läuft zum Ende
-        with patch('sys.stdin', new = StringIO('A\n1\n2\n3')):
+            self.assertRaises(Exception, dices(0))
+#Falscheingabe Zeichenkette, Float, außerhalb der zugelassen range - Programm läuft normal weiter
+        with patch('sys.stdin', new = StringIO('_as\n1.5\n80\n-1\n3\n4\n0')):
             with self.assertRaises(Exception):
-                dice_functions.dices(0)
+                dices(0)
--- kniffel_chris/test_file_handling.py
+++ kniffel_chris/test_file_handling.py
@@ -1,23 +1,40 @@
 from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
-import file_handling
+from file_handling import *
 
 class test_read_file_player(TestCase):
 	def test_read_file_kniffel_player(self):
 		with patch('sys.stdout', new = StringIO()):
-			self.assertRaises(Exception, file_handling.read_file_kniffel_player())
+			self.assertRaises(Exception, read_file_kniffel_player())
 	
 	def test_read_file_dice(self):
 		with patch('sys.stdout', new = StringIO()):
-			self.assertRaises(Exception, file_handling.read_file_dice())
+			self.assertRaises(Exception, read_file_dice())
 	
 	def test_write_file_player(self):
 		player = [["Caro",0,"-",6,0,5,18,"-","-","-","-","-","-","-","-","-","-","-","-"],["Nico",1,"-","-","-",10,12,"-","-","-","-","-","-",30,40,"-","-","-","-"]]
 		with patch('sys.stdout', new = StringIO()):
-			self.assertRaises(Exception, file_handling.write_file_player(player))
+			self.assertRaises(Exception, write_file_player(player))
 	
 	def test_write_file_dice(self):
 		dice_all = [5,4,1,6,4]
 		with patch('sys.stdout', new = StringIO()):
-			self.assertRaises(Exception, file_handling.write_file_dice(dice_all))
+			self.assertRaises(Exception, write_file_dice(dice_all))
+	
+	#Problem: rename Funktion erstellt immer eine neue Datei, und benennt das nicht um 
+	"""def test_read_file_player_exception(self):
+		os.rename('kniffel_player.json', 'file_not_available')
+		with self.assertRaises(Exception):
+			read_file_kniffel_player()"""
+	#Problem: Error wird nicht geworfen
+	"""def test_read_file_player_ValueError(self):
+		player = 5
+		with open ('kniffel_player.json', 'w', encoding='utf8') as kniffel_player:
+			json.dump(player, kniffel_player, indent=4)
+		with patch('sys.stdout', new = StringIO()):
+			with self.assertRaises(ValueError):
+				read_file_kniffel_player()
+		player = [["Caro",0,"-",6,0,5,18,"-","-","-","-","-","-","-","-","-","-","-","-"],["Nico",1,"-","-","-","ABC",12,"-","-","-","-","-","-",30,40,"-","-","-","-"]]
+		write_file_player(player)"""
+		
